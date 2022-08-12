9116c86b518a5618198429f1d48f5964533709e0
Chris DeHaan <cdehaan@gmail.com>
2022-08-13 07:42:18 +0900

    Initial commit

---
+++ App.js
@@ -0,0 +1,115 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { createContext, useEffect, useState } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Dimensions,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Header from './Header';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const [windowMaxDimensions, setWindowMaxDimensions] = useState({width: Dimensions.get('window').width, height: Dimensions.get('window').height});
+
+  useEffect(() => {
+      const subscription = Dimensions.addEventListener( "change",
+      ({ window }) =>
+          { setWindowMaxDimensions((previousWindowDimensions) =>
+              {return { width: Math.max(previousWindowDimensions.width, window.width), height: Math.max(previousWindowDimensions.height, window.height) }});
+          }
+      );
+      return () => subscription?.remove();
+  });
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const styles = StyleSheet.create({
+    mainView: {
+      height: windowMaxDimensions.height,
+      overflow: 'hidden',
+    },
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: '600',
+    },
+    sectionDescription: {
+      marginTop: 8,
+      fontSize: 18,
+      fontWeight: '400',
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+    debug: {
+      backgroundColor: "powderblue",
+      borderWidth: 5,
+      borderTopLeftRadius: 20,
+      borderTopRightRadius: 20,
+    },
+  });
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={[backgroundStyle, styles.debug, styles.mainView]}
+        contentContainerStyle={{justifyContent: 'space-evenly'}}>
+        <Header />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,74 @@
+import {Node, useEffect, useRef, useState} from 'react';
+import {Animated, Button, StyleSheet, Text, View, Dimensions} from 'react-native';
+import React from 'react';
+
+const Header = (): Node => {
+    const [windowMaxDimensions, setWindowMaxDimensions] = useState({width: Dimensions.get('window').width, height: Dimensions.get('window').height});
+
+    useEffect(() => {
+        const subscription = Dimensions.addEventListener( "change",
+        ({ window }) =>
+            { setWindowMaxDimensions((previousWindowDimensions) =>
+                {return { width: Math.max(previousWindowDimensions.width, window.width), height: Math.max(previousWindowDimensions.height, window.height) }});
+            }
+        );
+        return () => subscription?.remove();
+    });
+  
+    const headerAnim = useRef(new Animated.Value(0)).current;
+
+    function ExpandHeader() {
+        Animated.timing(headerAnim, {
+            toValue: 1,
+            duration: 600,
+            useNativeDriver: false,
+        }).start();
+    };
+
+    function ShrinkHeader() {
+        Animated.timing(headerAnim, {
+            toValue: 0,
+            duration: 600,
+            useNativeDriver: false,
+        }).start();
+    };
+
+    const headerOpacity = headerAnim.interpolate({ inputRange:[0, 1], outputRange:[0.5,   1] });
+    const headerBorder  = headerAnim.interpolate({ inputRange:[0, 1], outputRange:[0,     5] });
+    const headerWidth   = headerAnim.interpolate({ inputRange:[0, 1], outputRange:['0%', '100%'] });
+
+    const styles = StyleSheet.create({
+        header: {
+            opacity: headerOpacity,
+            width: headerWidth,
+            borderWidth: headerBorder,
+            height: 100,
+            alignSelf: 'center',
+            overflow: 'hidden',
+            backgroundColor: 'beige',
+        },
+        debug: {
+            backgroundColor: "powderblue",
+            borderWidth: 5,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+        }
+    });
+
+    return (
+        <View>
+        <Animated.View style={styles.header}>
+            <View style={{width: windowMaxDimensions.width, backgroundColor: "#336699"}}>
+            <Text>
+                This is my header.
+            </Text>
+            </View>
+        </Animated.View>
+        <Button title="Fade In Header" onPress={ExpandHeader} />
+        <Button title="Fade Out Header" onPress={ShrinkHeader} />
+        </View>
+    );
+};
+  
+  export default Header;
+          
